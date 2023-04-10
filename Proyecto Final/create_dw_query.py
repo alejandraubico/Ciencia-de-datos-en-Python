016a389ec70c515e5f16ac25a6462c3133bb24de
alejandraubico <124924293+alejandraubico@users.noreply.github.com>
2023-04-09 19:01:46 -0600

    Add files via upload

--- Proyecto Final/create_dw_query.py
+++ Proyecto Final/create_dw_query.py
@@ -1,7 +1,7 @@
 CREATE_DW =  """
 CREATE TABLE IF NOT EXISTS dimShipStatus(
 	ShipStatusID INT NOT NULL AUTO_INCREMENT,
-	ShipStatusName VARCHAR(50), 
+	ShipStatusName VARCHAR(50) 
     PRIMARY KEY(ShipStatusID)
 );
 
@@ -66,4 +66,66 @@ create table if not exists dimDate(
     day int     
 );
 
+create table if not exists factOrders(
+    OrderIndexID varchar(100) primary key,
+    DateID date,
+    ShipStatusID int,
+    FulfillmentID int,
+    SalesChannelID int,
+    ShipServiceID int,
+    ProductID int,
+    CourierStatusID int,
+    CurrencyID int,
+    PromotionID int,
+    FulfillmentByID int,
+    ShippingAddressID int,
+    Quantity varchar(75),
+    Amount double,
+    B2B varchar(10),
+
+    constraint fk_fact_dimShipStatus
+        foreign key (ShipStatusID)
+            references dimShipStatus(ShipStatusID),
+
+    constraint fk_fact_dimFulfillment
+        foreign key (FulfillmentID)
+            references dimFulfillment(FulfillmentID),
+
+    constraint fk_fact_dimSalesChannel
+        foreign key (SalesChannelID)
+            references dimSalesChannel(SalesChannelID),
+
+    constraint fk_fact_dimShipService
+        foreign key (ShipServiceID)
+            references dimShipService(ShipServiceID),
+
+    constraint fk_fact_dimProduct
+        foreign key (ProductID)
+            references dimProduct(ProductID),
+
+    constraint fk_fact_dimCourierStatus
+        foreign key (CourierStatusID)
+            references dimCourierStatus(CourierStatusID),
+
+    constraint fk_fact_dimCurrency
+        foreign key (CurrencyID)
+            references dimCurrency(CurrencyID),
+
+    constraint fk_fact_dimPromotion
+        foreign key (PromotionID)
+            references dimPromotion(PromotionID),
+
+    constraint fk_fact_dimFulfillmentBy
+        foreign key (FulfillmentByID)
+            references dimFulfillmentBy(FulfillmentByID),
+
+    constraint fk_fact_dimShippingAddress
+        foreign key (ShippingAddressID)
+            references dimShippingAddress(ShippingAddressID),
+
+    constraint fk_fact_dimDate
+        foreign key (DateID)
+            references dimDate(DateID)
+
+);
 """
